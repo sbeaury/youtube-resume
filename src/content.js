77e399cab9901489524c67e2c2b96fc01c4a86b8
kumustaka <sbeaury@gmail.com>
2020-03-22 20:43:27 -0400

    add local storage + background css

--- src/content.js
+++ src/content.js
@@ -5,14 +5,23 @@ import ReactDOM from "react-dom";
 import ReactPlayer from "react-player";
 import { css } from "emotion";
 
+async function getLocalStorageValue(key) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.sync.get(key, function(value) {
+        resolve(value);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      arrayVideos: [],
-      url: "",
-      currentMin: 0,
-      currentSec: 0
+      videos: []
     };
   }
 
@@ -22,7 +31,7 @@ class Main extends React.Component {
     };
 
     chrome.tabs.query(queryInfo, result => {
-      let arrayVideosCopy = [];
+      let arrayVideos = [];
       for (let i = 0; i < result.length; i++) {
         let url = result[i].url;
         console.log(url);
@@ -33,23 +42,50 @@ class Main extends React.Component {
             const time = results && results[0];
             const minute = Math.round(time / 60);
             const second = Math.round(time % 60);
-            arrayVideosCopy.push({
+            arrayVideos.push({
               url: url,
               currentMin: minute,
               currentSec: second
             });
-            this.setState({ arrayVideos: arrayVideosCopy });
-            console.log(this.state.arrayVideos);
+
+            this.setState({ videos: arrayVideos });
+
+            const { videos } = this.state;
+
+            chrome.storage.sync.set(
+              { myKey: videos },
+              console.log("Video array saved!")
+            );
           }
         );
       }
     });
+
+    getLocalStorageValue("myKey").then(result => {
+      this.setState({ videos: result.myKey });
+    });
   }
 
   render() {
-    const { arrayVideos } = this.state;
+    const { videos } = this.state;
     return (
-      <>
+      <div
+        className={css`
+          background: #e0eafc; /* fallback for old browsers */
+          background: -webkit-linear-gradient(
+            to left,
+            #cfdef3,
+            #e0eafc
+          ); /* Chrome 10-25, Safari 5.1-6 */
+          background: linear-gradient(
+            to left,
+            #cfdef3,
+            #e0eafc
+          ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+          color: black;
+        `}
+      >
         <div
           className={css`
             display: flex;
@@ -58,21 +94,24 @@ class Main extends React.Component {
           `}
         >
           <div
-          className={css`
-            margin:auto;
-          `}>
-            <img src="https://img.icons8.com/dusk/2x/youtube-play.png" />
-          </div>
-          <div className={css`
-            margin:auto;
-          `}>
-          <h1
             className={css`
-              font-family: "Open Sans";
+              margin: auto;
             `}
           >
-            Don't let your YouTube browsing history fade away
-          </h1>
+            <img src="https://img.icons8.com/dusk/2x/youtube-play.png" />
+          </div>
+          <div
+            className={css`
+              margin: auto;
+            `}
+          >
+            <h1
+              className={css`
+                font-family: "Open Sans";
+              `}
+            >
+              Don't let your YouTube browsing history fade away
+            </h1>
           </div>
         </div>
         <div
@@ -81,22 +120,26 @@ class Main extends React.Component {
             justify-content: center;
           `}
         >
-          {arrayVideos.map(object => {
-            return (
-              <ReactPlayer
-                url={`${object.url}&t=${object.currentMin}m${object.currentSec}s`}
-                className={css`
+          {videos ? (
+            videos.map(object => {
+              return (
+                <ReactPlayer
+                  url={`${object.url}&t=${object.currentMin}m${object.currentSec}s`}
+                  className={css`
                   width: 20%;
                   margin: 1rem;
                   border: 3px solid #333333;
                   border-radius: 2% 6% 5% 4% / 1% 1% 2% 4%;
                   }
                 `}
-              />
-            );
-          })}
+                />
+              );
+            })
+          ) : (
+            <p>Something went wrong ¯\_(ツ)_/¯</p>
+          )}
         </div>
-      </>
+      </div>
     );
   }
 }
