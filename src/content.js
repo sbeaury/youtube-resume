12848f2b6b585fc7af3a98048d909959d9fb4d0c
kumustaka <sbeaury@gmail.com>
2020-03-29 11:57:18 -0400

    fix bugs when result is empty

--- src/content.js
+++ src/content.js
@@ -4,8 +4,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import ReactPlayer from "react-player";
 import { css } from "emotion";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-async function getLocalStorageValue(key) {
+const getLocalStorageValue = async key => {
   return new Promise((resolve, reject) => {
     try {
       chrome.storage.sync.get(key, function(value) {
@@ -15,7 +17,17 @@ async function getLocalStorageValue(key) {
       reject(error);
     }
   });
-}
+};
+
+const removeDuplicates = arrayOfObjects => {
+  return arrayOfObjects.filter(
+    (object, index) =>
+      index ===
+      arrayOfObjects.findIndex(
+        obj => JSON.stringify(obj) === JSON.stringify(object)
+      )
+  );
+};
 
 class Main extends React.Component {
   constructor(props) {
@@ -26,61 +38,75 @@ class Main extends React.Component {
   }
 
   componentDidMount() {
+    getLocalStorageValue("myKey").then(result => {
+      if (result.myKey.length > 0) {
+        this.setState({ videos: result.myKey });
+      } else {
+        this.setState({ videos: [] });
+      }
+    });
+
     const queryInfo = {
       url: "*://www.youtube.com/*"
     };
 
     chrome.tabs.query(queryInfo, result => {
-      let arrayVideos = [...this.state.videos];
-      for (let i = 0; i < result.length; i++) {
-        let url = result[i].url;
-        console.log(url);
-        chrome.tabs.executeScript(
-          result[i].id,
-          { code: 'document.querySelector("video").currentTime' },
-          results => {
-            const time = results && results[0];
-            const minute = Math.round(time / 60);
-            const second = Math.round(time % 60);
-            arrayVideos.push({
-              url: url,
-              currentMin: minute,
-              currentSec: second
-            });
+      let arrayVideos = removeDuplicates([...this.state.videos]);
+      console.log(result);
+      if (result) {
+        for (let i = 0; i < result.length; i++) {
+          let url = result[i].url;
+          console.log(url);
+          chrome.tabs.executeScript(
+            result[i].id,
+            { code: 'document.querySelector("video").currentTime' },
+            results => {
+              const time = results && results[0];
+              const minute = Math.round(time / 60);
+              const second = Math.round(time % 60);
+              arrayVideos.push({
+                url: url,
+                currentMin: minute,
+                currentSec: second
+              });
 
-            this.setState({ videos: arrayVideos });
+              this.setState({ videos: arrayVideos });
 
-            chrome.storage.sync.set(
-              { myKey: this.state.videos },
-              console.log("Video array saved!")
-            );
-          }
-        );
+              chrome.storage.sync.set(
+                { myKey: this.state.videos },
+                console.log("Video array saved!")
+              );
+            }
+          );
+        }
       }
     });
-
-    getLocalStorageValue("myKey").then(result => {
-      this.setState({ videos: result.myKey });
-    });
   }
+
+  handleClearCache = () => {
+    chrome.storage.sync.remove("myKey", () => {
+      console.log("Cache has been cleared");
+    });
+  };
 
   render() {
     const { videos } = this.state;
     return (
       <div
         className={css`
-          background: #e0eafc; /* fallback for old browsers */
-          background: -webkit-linear-gradient(
-            to left,
-            #cfdef3,
-            #e0eafc
-          ); /* Chrome 10-25, Safari 5.1-6 */
           background: linear-gradient(
-            to left,
-            #cfdef3,
-            #e0eafc
-          ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-
+            to top,
+            #eb7c9c,
+            #d677a1,
+            #bf73a2,
+            #a870a1,
+            #916c9c
+          );
+          background-repeat: no-repeat;
+          background-size: cover;
+          width: 100%;
+          height: 100%;
+          background-attachment: fixed
           color: black;
         `}
       >
@@ -111,11 +137,29 @@ class Main extends React.Component {
               Don't let your YouTube browsing history fade away
             </h1>
           </div>
+          {videos.length === 0 ? null : (
+            <div
+              className={css`
+                display: flex;
+                justify-content: center;
+              `}
+            >
+              <IconButton aria-label="delete">
+                <DeleteIcon onClick={this.handleClearCache} />
+              </IconButton>
+            </div>
+          )}
         </div>
         <div
           className={css`
             display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            width: 90%;
+            height: 80vh;
             justify-content: center;
+            margin: auto;
+            background-size: cover;
           `}
         >
           {videos ? (
@@ -123,19 +167,34 @@ class Main extends React.Component {
               return (
                 <ReactPlayer
                   url={`${object.url}&t=${object.currentMin}m${object.currentSec}s`}
+                  width="20%"
+                  height="20%"
                   className={css`
-                  width: 20%;
                   margin: 1rem;
-                  border: 3px solid #333333;
-                  border-radius: 2% 6% 5% 4% / 1% 1% 2% 4%;
+                  border: 3px solid #333333;          
                   }
                 `}
                 />
               );
             })
           ) : (
-            <p>Something went wrong ¯\_(ツ)_/¯</p>
+            <p
+              className={css`
+                margin: auto;
+              `}
+            >
+              Something went wrong ¯\_(ツ)_/¯
+            </p>
           )}
+          {videos.length === 0 ? (
+            <div
+              className={css`
+                margin: auto;
+              `}
+            >
+              <img src="https://img.icons8.com/nolan/2x/empty-box.png" />
+            </div>
+          ) : null}
         </div>
       </div>
     );
